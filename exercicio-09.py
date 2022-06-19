da9acc1e63e1b6381fafaa7a2bfdd6b3ef4a325a
fernandoclbarros <fernandobarros@hotmail.com.br>
2022-06-19 18:08:11 -0300

    segundo commit

--- exercicio-09.py
+++ exercicio-09.py
@@ -7,13 +7,11 @@ def numeroPerfeito(numero):
     for contador in range(1, numero):
         if numero % contador == 0:
             listaMultiplos.append(contador)
-    
-    somaLista = sum(listaMultiplos)
+            somaLista = sum(listaMultiplos)
 
     if somaLista == numero:
         print(f'{numero} é um número perfeito')
     else:
         print(f'{numero} NÃO é um número perfeito')
-
-
+            
 numeroPerfeito(33550336)
