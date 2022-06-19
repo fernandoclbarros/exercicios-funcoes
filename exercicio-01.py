b173b0ced14a9896088bc8109d852c65b4cd4da1
fernandoclbarros <fernandobarros@hotmail.com.br>
2022-06-19 13:55:33 -0300

    primeiro commit exercicio 01

---
+++ exercicio-01.py
@@ -0,0 +1,6 @@
+#Escreva um programa que execute uma função que encontre o maior número de uma lista passada por parâmetro.
+
+def encontrarNumMaior(lista):
+    return print(max(lista))
+
+encontrarNumMaior ([10, 20, 25, 980, 1230, 30, 500, 620])
